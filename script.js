a5948571477a0fba5f3faa7bcd92bb5368b023d8
Student <student@Cumberlandss-MacBook-Pro.local>
2021-09-23 02:27:29 -0500

    Completed the LiteralMathMethodsExercise with Bonus and Extra Bonus

---
+++ script.js
@@ -0,0 +1,59 @@
+// LiteralMathMethodExercise
+
+// #1a
+let warmHugs = `Hi, I'm Olof and I like warm hugs`;
+console.log(warmHugs);
+
+// #1b
+let text = "warmHugs";
+console.log(text.toUpperCase());
+
+// #1c
+let like = "love";
+console.log(like.replace("l","l"));
+console.log(warmHugs.replace("like","love"));
+
+// #2a
+let beenImpaled = "Oh, look at that, I've been impaled.";
+console.log(beenImpaled);
+
+// #2b
+let breakTime = "Oh, look at that. I've been impaled.";
+console.log(beenImpaled.slice(18, 36));
+
+// #3a
+let dotDotDot ="..."
+console.log("dotDotDot");
+
+// #3b
+let skullBones =`I don't have a skull${dotDotDot} or bones.`
+console.log(skullBones);
+// #4
+Math.PI;
+console.log(Math.PI);
+
+// #5
+let randomNumber = Math.random();randomNumber *=3;
+console.log(randomNumber);
+
+// Bonus
+
+// #6
+let letItGo = " Let It Go!"
+console.log(letItGo.toUpperCase().repeat(2));
+
+// #7a
+let reindeers = ("Reindeers are better than people.");
+console.log(reindeers);
+
+// #7b
+reindeers = "Reindeers_are_better_than_people."
+console.log(reindeers);
+
+// #8
+Math.sqrt(2)
+console.log(Math.sqrt(2));
+
+// #9
+let newRandomNumber = Math.floor(Math.random()*(23-7));
+console.log(newRandomNumber);
